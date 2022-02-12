b2291718a584baeb17dca0fd8de34f4fb3734c97
Liwei Ma <Liwei.Ma@gmail.com>
2022-02-13 00:58:01 +0800

    github action

--- src/test/scala/test.scala
+++ src/test/scala/test.scala
@@ -4,22 +4,15 @@ import org.scalatest.freespec.{AnyFreeSpec}
 
 class FooTester extends AnyFreeSpec with BeforeAndAfterAllConfigMap{
 
-  private var value = "Hello World"
   private val key = "key"
+  private var value = "Hello World"
 
   override def beforeAll(configMap: ConfigMap) {
-    assume(
-      configMap.isDefinedAt(key),
-      "Config key: " + key
-    )
+    assume(configMap.isDefinedAt(key), "Config key: " + key)
     value = configMap.getRequired[String](key)
   }
 
-  // Delete the temp file
-  override def afterAll(configMap: ConfigMap) {
-  }
-
-  "Basic test in chiseltest" in {
+  "Basic test" in {
     println(value)
   }
 }
