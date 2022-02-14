1f2aa4e63bf0d5f8eb164b3db1201442a5993bf4
Liwei Ma <Liwei.Ma@gmail.com>
2022-02-14 20:22:08 +0800

    java process not see the key

--- src/test/scala/test.scala
+++ src/test/scala/test.scala
@@ -8,6 +8,13 @@ class FooTester extends AnyFreeSpec with BeforeAndAfterAllConfigMap{
   private var value = "Hello World"
 
   override def beforeAll(configMap: ConfigMap) {
+
+
+    println(System.getProperty("java.version"))
+    println(System.getProperty("java.runtime.version"))
+    println(System.getProperty("key"))
+    println(sys.env.get("key"))
+
     assume(configMap.isDefinedAt(key), "Config key: " + key)
     value = configMap.getRequired[String](key)
   }
